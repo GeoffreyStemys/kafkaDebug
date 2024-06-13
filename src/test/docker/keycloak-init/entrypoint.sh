3aa035525761d6d18af9c4acb2bc1439b3078070
Geoffrey Vincent <geoffrey.vincent@stemys.io>
2024-06-13 09:01:46 +0200

    debug kafka

--- src/test/docker/keycloak-init/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-
-ash -c "while ! wget -q --spider ${KEYCLOAK_URL}/realms/master/account/; do sleep 1; done;"
-
-terraform init
-
-terraform apply -auto-approve -var="keycloak_username=${KEYCLOAK_ADMIN_USER}" -var="keycloak_password=${KEYCLOAK_ADMIN_PASSWORD}" -var "keycloak_host=${KEYCLOAK_URL}"
